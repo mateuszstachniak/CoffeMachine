e7962712026d2945a8bb4e854e7016852de606cd
mateuszstachniak <91155660+mateuszstachniak@users.noreply.github.com>
2021-11-25 23:48:33 +0100

    Add loop and check resources

--- Main.java
+++ Main.java
@@ -6,20 +6,24 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] coffeeMachineIngredients = {400, 540, 120, 9, 550};
-        CoffeeMachineStatus(coffeeMachineIngredients);
-        System.out.println("Write action (buy, fill, take): ");
-        String action = scanner.next();
-        if (action.equals("buy")) {
-            coffeeBuying(coffeeMachineIngredients);
-        } else if (action.equals("fill")) {
-            fillMachine(coffeeMachineIngredients);
-        } else if (action.equals("take")) {
-            giveMoney(coffeeMachineIngredients);
-        }
-        CoffeeMachineStatus(coffeeMachineIngredients);
+        do {
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            String action = scanner.next();
+            if (action.equals("buy")) {
+                coffeeBuying(coffeeMachineIngredients);
+            } else if (action.equals("fill")) {
+                fillMachine(coffeeMachineIngredients);
+            } else if (action.equals("take")) {
+                giveMoney(coffeeMachineIngredients);
+            } else if (action.equals("remaining")) {
+                remaining(coffeeMachineIngredients);
+            } else if (action.equals("exit")) {
+                break;
+            }
+        } while (true);
     }
     
-    public static void CoffeeMachineStatus(int[] coffeeMachineIngredients) {
+    public static void remaining(int[] coffeeMachineIngredients) {
         System.out.println("The caffe machine has: ");
         System.out.println(coffeeMachineIngredients[0] + " ml of water");
         System.out.println(coffeeMachineIngredients[1] + " ml of milk");
@@ -27,44 +31,39 @@ public class CoffeeMachine {
         System.out.println(coffeeMachineIngredients[3] + " disposable cups");
         System.out.println("$" + coffeeMachineIngredients[4] + " of money");
         System.out.println();
-    } 
+    }
     
     public static void coffeeBuying(int[] coffeeMachineIngredients) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
-        int choice = scanner.nextInt();
-        if (choice == 1) {
+        System.out.println("\n" +
+                "What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
+        String choice = scanner.next();
+        if (choice.equals("1")) {
             espresso(coffeeMachineIngredients);
-        } else if (choice == 2) {
+        } else if (choice.equals("2")) {
             latte(coffeeMachineIngredients);
-        } else if (choice == 3) {
+        } else if (choice.equals("3")) {
             cappucino(coffeeMachineIngredients);
+        } else if (choice.equals("back")) {
+
         }
     }
-    
+
     public static int[] espresso(int[] coffeeMachineIngredients) {
-        coffeeMachineIngredients[0] -= 250;
-        coffeeMachineIngredients[2] -= 16;
-        coffeeMachineIngredients[3]--;
-        coffeeMachineIngredients[4] += 4;
+        int[] ingredients = {250, 0, 16, 1, 4};
+        makeCoffee(coffeeMachineIngredients, ingredients);
         return coffeeMachineIngredients;
     }
 
     public static int[] latte(int[] coffeeMachineIngredients) {
-        coffeeMachineIngredients[0] -= 350;
-        coffeeMachineIngredients[1] -= 75;
-        coffeeMachineIngredients[2] -= 20;
-        coffeeMachineIngredients[3]--;
-        coffeeMachineIngredients[4] += 7;
+        int[] ingredients = {350, 75, 20, 1, 7};
+        makeCoffee(coffeeMachineIngredients, ingredients);
         return coffeeMachineIngredients;
     }
 
     public static int[] cappucino(int[] coffeeMachineIngredients) {
-        coffeeMachineIngredients[0] -= 200;
-        coffeeMachineIngredients[1] -= 100;
-        coffeeMachineIngredients[2] -= 12;
-        coffeeMachineIngredients[3]--;
-        coffeeMachineIngredients[4] += 6;
+        int[] ingredients = {200, 100, 12, 1, 6};
+        makeCoffee(coffeeMachineIngredients, ingredients);
         return coffeeMachineIngredients;
     }
 
@@ -87,5 +86,43 @@ public class CoffeeMachine {
         System.out.println("I gave you $" + earnings);
         System.out.println();
         return coffeeMachineIngredients;
-    }               
+    }
+
+    public static int[] makeCoffee(int[] coffeeMachineIngredients, int[] ingredients) {
+        boolean noResources = false;
+        int resource = 0;
+        for (int i = 0; i < coffeeMachineIngredients.length; i++) {
+            if (coffeeMachineIngredients[i] < ingredients[i]) {
+                noResources = true;
+                resource = i;
+                break;
+            }
+        }
+        if (noResources) {
+            switch (resource) {
+                case 0:
+                    System.out.println("Sorry, not enough water!");
+                    break;
+                case 1:
+                    System.out.println("Sorry, not enough milk!");
+                    break;
+                case 2:
+                    System.out.println("Sorry, not enough coffee beans!");
+                    break;
+                case 3:
+                    System.out.println("Sorry, not enough disposable cups!");
+                    break;
+            }
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+        }
+        if (!noResources) {
+            coffeeMachineIngredients[0] -= ingredients[0];
+            coffeeMachineIngredients[1] -= ingredients[1];
+            coffeeMachineIngredients[2] -= ingredients[2];
+            coffeeMachineIngredients[3] -= ingredients[3];
+            coffeeMachineIngredients[4] += ingredients[4];
+        }
+        return coffeeMachineIngredients;
+    }
 }
--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 # Assumption
 
 Now the program allows the user to:\
--make coffee, fill resources of the machine, take profit\
--when user want to make coffee he can choose between espresso, latte and cappucino\
+-make coffee, fill resources of the machine, take profit, see the remaining resources and exit program\
+-when user want to make coffee he can choose between espresso, latte and cappuccino\
+-if there is no recources program will inform about it and will not make coffee
+-program working in a loop
 
 
